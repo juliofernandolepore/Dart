67b5dae1ee75a7a137751837b4fffac4e49167bf
FernandoLepore <juliofernandolepore@gmail.com>
2022-10-24 01:00:32 -0300

    comenzando a trabajar con MAPS

---
+++ maps.dart
@@ -0,0 +1,9 @@
+/*  coleccion MAPS: estas estructuras se manejan con :
+LLAVE(KEY) Y VALUE (VALOR), no se manejan con indices como las listas */
+void main() {
+  var mapa1 = new Map();
+  print(mapa1);
+
+  var mapa2 = {"llave1": "juan", "llave2": "pedro"};
+  print(mapa2);
+}
