63eb7f377684b86eaa3ee86c3e83bba50f1e575c
FernandoLepore <juliofernandolepore@gmail.com>
2022-10-23 23:37:22 -0300

    metodos aplicables en dart

--- colecciones.dart
+++ colecciones.dart
@@ -1,6 +1,4 @@
-//colecciones en dart
-
-//listas (arreglos)
+/* colecciones en dart -   listas (arreglos) */
 
 List<int> lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lista2 = ["john", "peter", 3, true, "mark"];
@@ -45,6 +43,56 @@ void main() {
   print(nombres3);
 
   /* creacion de Listas con dimension FIJA */
-  List numeros = List();
+  List numeros = List<int>.filled(5, 0);
+  /* esta es una lista de 5 elemento con valor 0 cada uno */
+
   print(numeros);
+  /* numeros.add(50);  aqui no deja agregar mas capacidad a la lista*/
+  numeros[0] = 80;
+  print(numeros);
+
+  /*    METODOS DE LAS LISTAS  */
+  /* este ejemplo usare "getRange" que es para iterar. al primer argumento
+  lo incluye pero en el segundo argumento(no se cuenta) es hasta el 
+  elemento anterior */
+
+  print(nombres3.getRange(1, 3));
+
+  /* ahora uso el metodo setAll para incorporar a la lista en el indice
+  que considere adecuado*/
+  nombres3.setAll(0, ["incorporado1", "incorporado2"]);
+  print(nombres3);
+
+  /*      ahora practicando el metodo "foreach" en listas */
+  nombres3.forEach((receptor) {
+    return print(receptor);
+  });
+  /* otra forma de escribir la funcion dentro del metodo es la siguiente,
+  en este caso hago uso de "arrow function o funcion flecha" */
+  nombres3.forEach((receptor) => print(receptor));
+
+  /*  LISTAS DINAMICAS: es una lista que permite almacenar datos de 
+  distintos tipos.  */
+  List<dynamic> miListaDinamica = [];
+  print(miListaDinamica);
+
+  /*   PROPIEDADES Y METODOS UTILES EN DART*/
+  print(nombres.length); //devuelve la cantidad de elementos que contiene.
+  //ahora revertir el orden de los elementos.
+  print(nombres.reversed);
+  /* ahora a practicar con el metodo "  insert"   */
+  nombres.insert(3, "TERCERO");
+  print(nombres);
+  /* ahora a probar el metodo "insertAll" la cual inserta varios elementos */
+  nombres.insertAll(0, ["sub1", "sub2", "sub3"]);
+  print(nombres);
+  /* ahora a probar un metodo para remover un valor en un indice determinado */
+  nombres.removeAt(6);
+  print(nombres);
+  /* ahora un metodo que elimina con target especifico */
+  nombres.remove("sub2");
+  print(nombres);
+  /* ahora un metodo con funcion o condicion a evaluar */
+  nombres.removeWhere((elemento) => elemento == "sub1");
+  print(nombres);
 }
